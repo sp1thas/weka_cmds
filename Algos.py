31708c96360cad357e4e24692ccee4f4f7acdf9d
sp1thas <sp1thas@autistici.org>
2017-01-07 17:16:20 +0200

    arguments from bash, debugging and output file

--- Algos.py
+++ Algos.py
@@ -1,221 +1,206 @@
 # -*- coding: utf-8 -*-
-import subprocess, sys, os
+import subprocess, sys, os, pprint
 
-def subprocess_cmd(command):
+def subprocess_cmd(command, output):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
-    print proc_stdout
-    divider()
+    print(proc_stdout.decode('utf-8', 'ignore'))
+    file = open(output,'w')
+    file.write(proc_stdout.decode('utf-8', 'ignore'))
+    file.close()
 
 ############## BAYES #################
 ######################################
 
-def BayesNet(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.bayes.BayesNet -t '
-    command += filename + ' -D -Q weka.classifiers.bayes.net.search.local.K2 -- -P 1 -S BAYES -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5'
-    subprocess_cmd(command)
-    divider()
+def BayesNet(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.bayes.BayesNet -t ' + filename +
+                ' -D -Q weka.classifiers.bayes.net.search.local.K2 -- -P 1 -S BAYES -E weka.classifiers.bayes.net.estimate.SimpleEstimator -- -A 0.5' )
+    subprocess_cmd(command, output)
 
-def NaiveBayes(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.bayes.NaiveBayes -t '
-    command += filename
-    subprocess_cmd(command)
+def NaiveBayes(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.bayes.NaiveBayes -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def NaiveBayesMultinomial(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.bayes.NaiveBayesMultinomial -t '
-    command += filename
-    subprocess_cmd(command)
+def NaiveBayesMultinomial(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.bayes.NaiveBayesMultinomial -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def NaiveBayesMultinomialText(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.bayes.NaiveBayesMultinomialText -t '
-    command += filename + ' -P 0 -M 3.0 -norm 1.0 -lnorm 2.0 -stopwords-handler weka.core.stopwords.Null -tokenizer "weka.core.tokenizers.WordTokenizer -delimiters \" \\r\\n\\t.,;:\\\'\\\"()?!\"" -stemmer weka.core.stemmers.NullStemmer'
-    subprocess_cmd(command)
-    divider()
+def NaiveBayesMultinomialText(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.bayes.NaiveBayesMultinomialText -t ' +
+                filename + ' -P 0 -M 3.0 -norm 1.0 -lnorm 2.0 -stopwords-handler weka.core.stopwords.Null -tokenizer "weka.core.tokenizers.WordTokenizer -delimiters \" \\r\\n\\t.,;:\\\'\\\"()?!\"" -stemmer weka.core.stemmers.NullStemmer' +
+                output )
+    subprocess_cmd(command, output)
 
-def NaiveBayesUpdateable(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.bayes.NaiveBayesMultinomialUpdateable -t '
-    command += filename
-    subprocess_cmd(command)
+def NaiveBayesUpdateable(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.bayes.NaiveBayesMultinomialUpdateable -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
 
 ############# META ###################
 ######################################
-def Bagging(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.meta.Bagging -P 100 -S 1 -num-slots 1 -I 10 -t '
-    command += filename + ' -W weka.classifiers.trees.REPTree -- -M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0'
-    subprocess_cmd(command)
-    divider()
+def Bagging(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.meta.Bagging -P 100 -S 1 -num-slots 1 -I 10 -t ' +
+                filename + ' -W weka.classifiers.trees.REPTree -- -M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0' )
+    subprocess_cmd(command, output)
 ################ MISC ################
 ######################################
-def InputMappedClassifier(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.misc.InputMappedClassifier -t '
-    command += filename + ' -I -trim -W weka.classifiers.rules.ZeroR'
-    subprocess_cmd(command)
-    divider()
+def InputMappedClassifier(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.misc.InputMappedClassifier -t ' +
+                filename + ' -I -trim -W weka.classifiers.rules.ZeroR' )
+    subprocess_cmd(command, output)
 
 ########### RULES ####################
 ######################################
-def DecisionTable(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.rules.DecisionTable -t '
-    command += filename + ' -X 1 -S "weka.attributeSelection.BestFirst -D 1 -N 5"'
-    subprocess_cmd(command)
-    divider()
+def DecisionTable(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.rules.DecisionTable -t ' +
+                filename + ' -X 1 -S "weka.attributeSelection.BestFirst -D 1 -N 5"' )
+    subprocess_cmd(command, output)
 
-def JRip(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.rules.JRip -F 3 -N 2.0 -O 2 -S 1 -t '
-    command += filename
-    subprocess_cmd(command)
+def JRip(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.rules.JRip -F 3 -N 2.0 -O 2 -S 1 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def OneR(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.rules.ZeroR -t '
-    command += filename
-    subprocess_cmd(command)
+def OneR(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.rules.ZeroR -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def PART(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.rules.PART -M 2 -C 0.25 -Q 1 -t '
-    command += filename
-    subprocess_cmd(command)
+def PART(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.rules.PART -M 2 -C 0.25 -Q 1 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
 
-def ZeroR(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.rules.OneR -B 6 -t '
-    command += filename
-    subprocess_cmd(command)
+def ZeroR(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.rules.OneR -B 6 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
 
 ###########FUNCTIONS##################
 ######################################
 
-def Logistic(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.functions.Logistic -t '
-    command += filename + ' -R 1.0E-8 -M -1 -num-decimal-places 4'
-    subprocess_cmd(command)
-    divider()
+def Logistic(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.functions.Logistic -t ' +
+                filename + ' -R 1.0E-8 -M -1 -num-decimal-places 4' )
+    subprocess_cmd(command, output)
 
-def MLP(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.functions.MultilayerPerceptron -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a -t '
-    command += filename
-    subprocess_cmd(command)
-    divider()
+def MLP(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.functions.MultilayerPerceptron -L 0.3 -M 0.2 -N 500 -V 0 -S 0 -E 20 -H a -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def SimpleLogistic(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.functions.SimpleLogistic -I 0 -M 500 -H 50 -W 0.0 -t '
-    command += filename
-    subprocess_cmd(command)
-    divider()
+def SimpleLogistic(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.functions.SimpleLogistic -I 0 -M 500 -H 50 -W 0.0 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def SMO(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.functions.SMO -t '
-    command += filename + ' -C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K "weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007" -calibrator "weka.classifiers.functions.Logistic -R 1.0E-8 -M -1 -num-decimal-places 4"'
-    subprocess_cmd(command)
-    divider()
+def SMO(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.functions.SMO -t ' +
+                ' -C 1.0 -L 0.001 -P 1.0E-12 -N 0 -V -1 -W 1 -K "weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007" -calibrator "weka.classifiers.functions.Logistic -R 1.0E-8 -M -1 -num-decimal-places 4"' )
+    subprocess_cmd(command, output)
 
 
 ############# LAZY ###################
 ######################################
 
-def IBk(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.lazy.IBk -t '
-    command += filename + ' -K 1 -W 0 -A "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.EuclideanDistance -R first-last\""'
-    subprocess_cmd(command)
-    divider()
+def IBk(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.lazy.IBk -t ' +
+                filename + ' -K 1 -W 0 -A "weka.core.neighboursearch.LinearNNSearch -A \\"weka.core.EuclideanDistance -R first-last\\""' )
+    subprocess_cmd(command, output)
 
-def KStar(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.lazy.KStar -B 20 -M a -t '
-    command += filename
-    subprocess_cmd(command)
-    divider()
+def KStar(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.lazy.KStar -B 20 -M a -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def LWL(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.lazy.LWL -t '
-    command += filename + ' -U 0 -K -1 -A "weka.core.neighboursearch.LinearNNSearch -A \"weka.core.EuclideanDistance -R first-last\"" -W weka.classifiers.trees.DecisionStump'
-    subprocess_cmd(command)
-    divider()
+def LWL(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.lazy.LWL -t ' +
+                filename + ' -U 0 -K -1 -A "weka.core.neighboursearch.LinearNNSearch -A \\"weka.core.EuclideanDistance -R first-last\\"" -W weka.classifiers.trees.DecisionStump' )
+    subprocess_cmd(command, output)
 
 
 ############ TREES ###################
 ######################################
 
-def DecisionStump(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.DecisionStump -t ' + filename
-    subprocess_cmd(command)
-    divider()
+def DecisionStump(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.DecisionStump -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def HoeffdingTree(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.HoeffdingTree -L 2 -S 1 -E 1.0E-7 -H 0.05 -M 0.01 -G 200.0 -N 0.0 -t ' + filename
-    subprocess_cmd(command)
-    divider()
+def HoeffdingTree(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.HoeffdingTree -L 2 -S 1 -E 1.0E-7 -H 0.05 -M 0.01 -G 200.0 -N 0.0 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def J48(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.J48 -C 0.25 -M 2 -t ' + filename
-    subprocess_cmd(command)
-    divider()
+def J48(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.J48 -C 0.25 -M 2 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def LMT(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.LMT -I -1 -M 15 -W 0.0 -t ' + filename
-    subprocess_cmd(command)
-    divider()
+def LMT(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.LMT -I -1 -M 15 -W 0.0 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def RandomForest(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.RandomForest -P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -t ' + filename
-    subprocess_cmd(command)
-    divider()
+def RandomForest(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.RandomForest -P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
 
-def RandomTree(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.RandomTree -K 0 -M 1.0 -V 0.001 -S 1 -t '
-    command += filename
-    subprocess_cmd(command)
-    divider()
+def RandomTree(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.RandomTree -K 0 -M 1.0 -V 0.001 -S 1 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
-def REPTree(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.trees.REPTree -M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0 -t '
-    command += filename
-    subprocess_cmd(command)
+def REPTree(classpath, filename, output):
+    command = ( classpath + ';' +
+                'java weka.classifiers.trees.REPTree -M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0 -t ' +
+                filename )
+    subprocess_cmd(command, output)
 
 
 ############ EXTRA ###################
 ######################################
 def RBFNetwork(classpath, filename):
-    command = classpath + ';'
-    command += 'java weka.classifiers.functions.RBFNetwork -B 2 -S 1 -R 1.0E-8 -M -1 -W 0.1 -t '
-    command += filename + '-W weka.classifiers.trees.REPTree -- -M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0'
-    subprocess_cmd(command)
-    divider()
+    command = ( classpath + ';' +
+                'java weka.classifiers.functions.RBFNetwork -B 2 -S 1 -R 1.0E-8 -M -1 -W 0.1 -t ' +
+                filename + '-W weka.classifiers.trees.REPTree -- -M 2 -V 0.001 -N 3 -S 1 -L -1 -I 0.0' )
+    subprocess_cmd(command, output)
 
 
 ######################################
 
 def divider():
-    print '''
-    ========================================================
-
-    ========================================================
-
-    '''
+    print ('''
+    ========================================================''')
--- WekaCommands.py
+++ WekaCommands.py
@@ -1,166 +1,201 @@
 # -*- coding: utf-8 -*-
-import os
+import os, getopt
 from Setup import *
 from Algos import *
 
 
-
-# setup weka's directory
-WekaDirectory = WekaDir()
-# setup the working directory
-WorkingDir = WorkingDir()
-# setup the file name
-FileName = FileName(WorkingDir)
-
-ClassPath = ClassPath(WekaDirectory)
-
-
-choise = ''
-
-while not choise == '0':
-    print 'Choise algorithm:'
-    print '''
-    bayes
-    ━━━━┓
-        ┠── "b1": BayesNet
-        ┠── "b2": NaiveBayes
-        ┠── "b3": NaiveBayesMultinomial
-        ┠── "b4": NaiveBayesMultinomialText
-        ┖── "b5": NaiveBayesUpdateable
-    functions
-    ━━━━┳━━━━
-        ┠── "f1": Logistic
-        ┠── "f2": MultilayerPerceptron
-        ┠── "f3": SimpleLogistic
-        ┖── "f4": SMO
-    lazy
-    ━━━━┓
-        ┠── "l1": IBk
-        ┠── "l2": KStar
-        ┖── "l3": LWL
-    meta
-    ━━━━┓
-        ┃   "m1": AdaBoostM1
-        ┃   "m2": AdditiveRegression
-        ┃   "m3": AttributeSelectedClassifier
-        ┠── "m4": Bagging
-        ┃   "m5": ClassificationViaRegression
-        ┃   "m6": CostSensitiveClassifier
-        ┃   "m7": CVParameterSelection
-        ┃   "m8": FilteredClassifier
-        ┃   "m9": IterativeClassifierOptimizer
-        ┃   "m10": LogitBoost
-        ┃   "m11": MultiClassClassifier
-        ┃   "m12": MultiClassClassifierUpdateable
-        ┃   "m13": MultiScheme
-        ┃   "m14": RandomCommittee
-        ┃   "m14": RandomizableFilteredClassifier
-        ┃   "m15": RandomSubSpace
-        ┃   "m16": RegressionByDiscretization
-        ┃   "m17": Stacking
-        ┃   "m18": Vote
-        ┃   "m19": WeightedistancesHandlerWrapper
-    misc
-    ━━━━┓
-        ┠── "mi1": InputMappedClassifier
-        ┃   "mi2": SerializedClaassifier
-    rules
-    ━━━━┓
-        ┠── "r1": DecisionTable
-        ┠── "r2": JRip
-        ┃   "r3": M5Rules
-        ┠── "r4": OneR
-        ┠── "r5": PART
-        ┖── "r6": ZeroR
-    trees
-    ━━━━┓
-        ┠── "t1": DecisionStump
-        ┠── "t2": HoeffdingTree
-        ┠── "t3": J48
-        ┠── "t4": LMT
-        ┃   "t5": M5P
-        ┠── "t6": RandomForest
-        ┠── "t7": RandomTree
-        ┖── "t8": REPTree
-    extra
-    ━━━━┓
-        ┖── "e1": RBFNetwork
-    EXIT
-    ━━━━┓
-        ┖── "0"
-    '''
-    choise = raw_input('>>> ')
-
-    # bayes codeblock
-    if choise[0]  == 'b':
-        if choise[1]  == 1: BayesNet(ClassPath, FileName)
-        elif choise[1]  == 2: NaiveBayes(ClassPath, FileName)
-        elif choise[1]  == 3: NaiveBayesMultinomial(ClassPath, FileName)
-        elif choise[1]  == 4: NaiveBayesMultinomialText(ClassPath, FileName)
-        elif choise[1]  == 5: NaiveBayesUpdateable(ClassPath, FileName)
-
-    # fuctions codeblock
-    elif choise[0]  == 'f':
-        if choise[1]  == '1': Logistic(ClassPath, FileName)
-        elif choise[1]  == '2': MLP(ClassPath, FileName)
-        elif choise[1]  == '3': SimpleLogistic(ClassPath, FileName)
-        elif choise[1]  == '4': SMO(ClassPath, FileName)
-
-    # lazy codeblock
-    elif choise[0]  == 'l':
-        if choise[1]  == '1': IBk(ClassPath, FileName)
-        elif choise[1]  == '2': KStar(ClassPath, FileName)
-        elif choise[1]  == '3': LWL(ClassPath, FileName)
-
-    # meta codeblock
-    elif choise[0]  == 'm' and len(a)>2:
-        if choise[1:]  == '1': pass
-        elif choise[1:]  == '2': pass
-        elif choise[1:]  == '3': pass
-        elif choise[1:]  == '4': Bagging(ClassPath, FileName)
-        elif choise[1:]  == '5': pass
-        elif choise[1:]  == '6': pass
-        elif choise[1:]  == '7': pass
-        elif choise[1:]  == '8': pass
-        elif choise[1:]  == '9': pass
-        elif choise[1:]  == '10': pass
-        elif choise[1:]  == '11': pass
-        elif choise[1:]  == '12': pass
-        elif choise[1:]  == '13': pass
-        elif choise[1:]  == '14': pass
-        elif choise[1:]  == '15': pass
-        elif choise[1:]  == '16': pass
-        elif choise[1:]  == '17': pass
-        elif choise[1:]  == '18': pass
-        elif choise[1:]  == '19': pass
-
-    # misc codeblock
-    elif choise[0:2] == 'mi':
-        if choise[2] == '1': pass
-        elif choise[2] == '2': pass
-
-    # rules codeblock
-    elif choise[0]  == 'r':
-        if choise[1]  == 1: pass
-        elif choise[1]  == 2: pass
-        elif choise[1]  == 3: pass
-        elif choise[1]  == 4: pass
-        elif choise[1]  == 5: pass
-        elif choise[1]  == 6: ZeroR(ClassPath, FileName)
-
-    # trees codeblock
-    elif choise[0]  == 't':
-        if choise[1] == '1': pass
-        elif choise[1] == '2': pass
-        elif choise[1] == '3': J48(ClassPath, FileName)
-        elif choise[1] == '4': pass
-        elif choise[1] == '5': pass
-        elif choise[1] == '6': RandomForest(ClassPath, FileName)
-        elif choise[1] == '7': RandomTree(ClassPath, FileName)
-        elif choise[1] == '8': REPTree(ClassPath, FileName)
-
-    elif choise  == 'e':
-        if choise[1] == '1': RBFNetwork(ClassPath, FileName)
+def main(argv):
+    try:
+       opts, args = getopt.getopt(argv,"hi:o:w:",["ifile=","ofile=","wekadir="])
+    except getopt.GetoptError:
+       print ('WekaCommands.py -i <inputfile> -o <outputfile> -w <wekadirectory>')
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print ('WekaCommands.py -i <inputfile> -o <outputfile> -w <wekadirectory>')
+          sys.exit()
+       elif opt in ("-i", "--ifile"):
+           if os.path.exists(arg):
+              FileName = arg
+       elif opt in ("-o", "--ofile"):
+           OutputFile = arg
+       elif opt in ("-w", "--wekadir"):
+           if arg.endswith('/') and os.path.exist(arg+'weka.jar'):
+               WekaDirectory = arg + 'weka.jar'
+           elif os.path.exists(arg+'/weka.jar'):
+               WekaDirectory = arg + '/weka.jar'
 
 
-print 'bye!'
+    print (FileName)
+
+    # # setup weka's directory
+    # WekaDirectory = WekaDir()
+    # # setup the working directory
+    # WorkingDir = WorkingDir()
+    # # setup the file name
+    # FileName = FileName(WorkingDir)
+
+    ClassPath = classpath(WekaDirectory)
+    goon = '1'
+    while goon:
+        try:
+            goon = raw_input("Press ENTER to continue...")
+        except NameError:
+            goon = input("Press ENTER to continue...")
+
+
+    choise = ''
+
+    if not choise:
+        print ('Choise algorithm:')
+        print ('''
+        bayes
+        ━━━━┓
+            ┠── "b1": BayesNet
+            ┠── "b2": NaiveBayes
+            ┠── "b3": NaiveBayesMultinomial
+            ┠── "b4": NaiveBayesMultinomialText
+            ┖── "b5": NaiveBayesUpdateable
+        functions
+        ━━━━┳━━━━
+            ┠── "f1": Logistic
+            ┠── "f2": MultilayerPerceptron
+            ┠── "f3": SimpleLogistic
+            ┖── "f4": SMO
+        lazy
+        ━━━━┓
+            ┠── "l1": IBk
+            ┠── "l2": KStar
+            ┖── "l3": LWL
+        meta
+        ━━━━┓
+            ┃   "m1": AdaBoostM1
+            ┃   "m2": AdditiveRegression
+            ┃   "m3": AttributeSelectedClassifier
+            ┠── "m4": Bagging
+            ┃   "m5": ClassificationViaRegression
+            ┃   "m6": CostSensitiveClassifier
+            ┃   "m7": CVParameterSelection
+            ┃   "m8": FilteredClassifier
+            ┃   "m9": IterativeClassifierOptimizer
+            ┃   "m10": LogitBoost
+            ┃   "m11": MultiClassClassifier
+            ┃   "m12": MultiClassClassifierUpdateable
+            ┃   "m13": MultiScheme
+            ┃   "m14": RandomCommittee
+            ┃   "m14": RandomizableFilteredClassifier
+            ┃   "m15": RandomSubSpace
+            ┃   "m16": RegressionByDiscretization
+            ┃   "m17": Stacking
+            ┃   "m18": Vote
+            ┃   "m19": WeightedistancesHandlerWrapper
+        misc
+        ━━━━┓
+            ┠── "mi1": InputMappedClassifier
+            ┃   "mi2": SerializedClaassifier
+        rules
+        ━━━━┓
+            ┠── "r1": DecisionTable
+            ┠── "r2": JRip
+            ┃   "r3": M5Rules
+            ┠── "r4": OneR
+            ┠── "r5": PART
+            ┖── "r6": ZeroR
+        trees
+        ━━━━┓
+            ┠── "t1": DecisionStump
+            ┠── "t2": HoeffdingTree
+            ┠── "t3": J48
+            ┠── "t4": LMT
+            ┃   "t5": M5P
+            ┠── "t6": RandomForest
+            ┠── "t7": RandomTree
+            ┖── "t8": REPTree
+        extra
+        ━━━━┓
+            ┖── "e1": RBFNetwork
+        EXIT
+        ━━━━┓
+            ┖── "0"
+        ''')
+        try:
+            choise = raw_input('>>> ')
+        except NameError:
+            choise = input('>>> ')
+        # bayes codeblock
+        if choise[0]  == 'b':
+            if choise[1]  == '1': BayesNet(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '2': NaiveBayes(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '3': NaiveBayesMultinomial(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '4': NaiveBayesMultinomialText(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '5': NaiveBayesUpdateable(ClassPath, FileName, OutputFile)
+
+        # fuctions codeblock
+        elif choise[0]  == 'f':
+            if choise[1]  == '1': Logistic(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '2': MLP(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '3': SimpleLogistic(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '4': SMO(ClassPath, FileName, OutputFile)
+
+        # lazy codeblock
+        elif choise[0]  == 'l':
+            if choise[1]  == '1': IBk(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '2': KStar(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '3': LWL(ClassPath, FileName, OutputFile)
+
+        # meta codeblock
+        elif choise[0]  == 'm':
+            if choise[1:]  == '1': pass
+            elif choise[1:]  == '2': pass
+            elif choise[1:]  == '3': pass
+            elif choise[1:]  == '4': Bagging(ClassPath, FileName, OutputFile)
+            elif choise[1:]  == '5': pass
+            elif choise[1:]  == '6': pass
+            elif choise[1:]  == '7': pass
+            elif choise[1:]  == '8': pass
+            elif choise[1:]  == '9': pass
+            elif choise[1:]  == '10': pass
+            elif choise[1:]  == '11': pass
+            elif choise[1:]  == '12': pass
+            elif choise[1:]  == '13': pass
+            elif choise[1:]  == '14': pass
+            elif choise[1:]  == '15': pass
+            elif choise[1:]  == '16': pass
+            elif choise[1:]  == '17': pass
+            elif choise[1:]  == '18': pass
+            elif choise[1:]  == '19': pass
+
+        # misc codeblock
+        elif choise[0:2] == 'mi':
+            if choise[2] == '1': InputMappedClassifier(ClassPath, FileName, OutputFile)
+            elif choise[2] == '2': pass
+
+        # rules codeblock
+        elif choise[0]  == 'r':
+            if choise[1]  == '1': DecisionTable(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '2': JRip(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '3': pass
+            elif choise[1]  == '4': OneR(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '5': PART(ClassPath, FileName, OutputFile)
+            elif choise[1]  == '6': ZeroR(ClassPath, FileName, OutputFile)
+
+        # trees codeblock
+        elif choise[0]  == 't':
+            if choise[1] == '1': DecisionStump(ClassPath,FileName,OutputFile)
+            elif choise[1] == '2': HoeffdingTree(ClassPath,FileName,OutputFile)
+            elif choise[1] == '3': J48(ClassPath, FileName, OutputFile)
+            elif choise[1] == '4': LMT(ClassPath, FileName, OutputFile)
+            elif choise[1] == '5': pass
+            elif choise[1] == '6': RandomForest(ClassPath, FileName, OutputFile)
+            elif choise[1] == '7': RandomTree(ClassPath, FileName, OutputFile)
+            elif choise[1] == '8': REPTree(ClassPath, FileName, OutputFile)
+
+        elif choise  == 'e':
+            if choise[1] == '1': RBFNetwork(ClassPath, FileName, OutputFile)
+
+        goformore = '1'
+
+    print ('bye!')
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
